664e077cb1ea0c0c2cb7e8ce5357b290e5e898aa
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-05-30 14:19:25 +0300

    sets

---
+++ sets.py
@@ -0,0 +1,17 @@
+markalar = {"Audi", "Mercedes", "BMW", "Honda"}
+
+
+# sonuc = markalar[0] error//
+
+sonuc = "BMW" in markalar
+
+markalar.add("Opel")
+markalar.update(["Toyota","Scoda"])
+
+sonuc = len(markalar)
+
+markalar.remove("Opel")
+
+# sonuc = markalar.pop() // random
+
+print(markalar)
