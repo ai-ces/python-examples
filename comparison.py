a3547e4453e53e4be3b59b0b41eb669df0940de3
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-05-30 14:39:08 +0300

    comparison

---
+++ comparison.py
@@ -0,0 +1,25 @@
+a,b,c d = 10, 10, 20,5 
+
+username = ""
+passowrd = ""
+
+sonuc = (a == b) # true
+sonuc = (a !=b) # false
+sonuc = (a == c) # true
+
+sonuc = (username == "")
+sonuc = (username == "")
+
+sonuc = (a > c) # false
+sonuc = (c > a) # true
+sonuc = (a >= b) # true
+sonuc = (a < c) # true
+sonuc =  (a <= b) # true
+sonuc = (True == 1) # true
+sonuc = (False == 0) # true
+sonuc = False + True # 1
+
+print(int(False))
+
+
+print(sonuc)
