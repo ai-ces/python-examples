afafff1815ed7e1e527e6915da63f3b6062b51ac
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-05-31 02:55:13 +0300

    if_elif_else

---
+++ if_elif_else.py
@@ -0,0 +1,24 @@
+
+
+# a = 10
+# b = 10
+
+# if (a > b):
+#     print("a b den buyuk")
+# elif (a == b):
+#     print("a b ye esit")
+# else:
+#     print("b a dan buyuk")
+
+
+
+karne_notu = 90
+
+if (karne_notu <50):
+    print("Sinif tekrari")
+elif (karne_notu >=50) and (karne_notu <70):
+    print("basarili")
+elif (karne_notu >=70) and (karne_notu <85):
+    print("tesekkur belgesi")
+else:
+    print("taktir belgesi")
