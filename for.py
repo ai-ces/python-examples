6d688e5508a0a13848e0a345757b39f2e11f6705
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-05-31 03:25:11 +0300

    for

---
+++ for.py
@@ -0,0 +1,26 @@
+# sayilar = [1,3,6,8,12,16,25]
+
+# for i in sayilar:
+#     print(i)
+
+# for i in sayilar:
+#     print("Merhaba")
+
+# _tuple = [(1,2),(3,4),(6,8)]
+
+# for a,b in _tuple:
+#     print(a,b)
+
+iller = {"01":"Ankara","02":"Adiyaman","03":"Afyon"}
+
+# for x in iller:
+#     print(x)
+
+# for x in iller:
+#     print(iller[x])
+
+# for x in iller.values():
+#     print(x)
+
+for key,value in iller.items():
+    print(key,value)
