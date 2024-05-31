67327d46a3a63e9e2c0b9c93ce6c84822994b38e
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-05-31 03:10:36 +0300

    uygulama 1

---
+++ uygulama_kosul_ifadeleri.py
@@ -0,0 +1,10 @@
+
+sayi = int(input("sayi giriniz:"))
+
+if (sayi > 0):
+    if (sayi % 2 == 1):
+        print("girilen sayi pozitif tek sayidir")
+    else:
+        print("girilen sayi pozitif cift sayidir")
+else:
+    print("Girilen sayi negatiftir")
