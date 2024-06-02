6b91a57048b8f2f11036181e1699d5fdc2de2ffa
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-03 01:51:03 +0300

    parameters

---
+++ parameter.py
@@ -0,0 +1,24 @@
+# a = 10
+# b = 10
+
+# def topla():
+#     return a + b
+
+# sonuc = topla(10,20)
+# print(sonuc)
+
+
+# def yasHesapla(dogumYili):
+#     return 2024 - dogumYili
+# sonuc = yasHesapla(1984)
+# sonuc = yasHesapla(1994)
+
+# print(sonuc)
+
+
+def selamla(isim):
+    return "Merhaba, " + isim
+
+sonuc = selamla("")
+
+print(sonuc)
