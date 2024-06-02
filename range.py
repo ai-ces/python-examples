9a0d41684b1b4f543a0b6f909d7eb724067897a0
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-02 17:42:45 +0300

    range

---
+++ range.py
@@ -0,0 +1,19 @@
+#liste = [4,8,11,18]
+
+#for i in liste:
+#    print(i)
+
+# r = range(10)
+# r = range(1,10)
+# r = range(1,10,2)
+
+# sonuc = list(r)
+
+# print(sonuc)
+
+# for i in range(10):
+#     print(i)
+
+for i in range(1,10):
+    if (i % 2 == 1):
+        print(i)
