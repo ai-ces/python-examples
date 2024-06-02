cff19dd168ffa84904c5bd44fc16750614406dd2
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-03 01:57:05 +0300

    default_parameters

---
+++ default_parametrers.py
@@ -0,0 +1,24 @@
+# def selamla(isim="Kullanici",mesaj="Hos Geldin"):
+#     print(f"{mesaj} {isim}")
+
+# selamla()
+
+
+# def usAlma(taban,us=2):
+#     return taban ** us
+
+# sonuc = usAlma(5)
+
+# print(sonuc)
+
+
+def carpma(a,b):
+    return a * b
+def toplama(a,b):
+    return a + b
+def islem(a,b):
+    return carpma(a,b)
+
+sonuc = islem(10,5)
+
+print(sonuc)
