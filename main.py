a9f10b5f3947ef2c6d8b38155a8e833f2bdcd180
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-05 22:38:41 +0300

    modules

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import module
+
+
+sonuc = module.password
+sonuc = module.user
+sonuc = module.user["notlar"]
+
+sonuc = module.ortalama(100,50)
+
+print(sonuc)
---
+++ module.py
@@ -0,0 +1,12 @@
+password = "123456"
+
+sayilar = [1,8,3,24,35]
+
+user = {
+    "username":"aaa",
+    "password":"123456",
+    "notlar": [60,90,85]
+}
+
+def ortalama(a,b):
+    return (a+b)/2
